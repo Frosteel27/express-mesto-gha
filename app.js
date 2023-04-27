cbf45942ef3914102790e7042f74a260cd2b20a1
frosteel27 <frosteel27@yandex.ru>
2023-04-27 15:05:50 +0700

    feat: deploy preparation

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const routes = require('./routes/index');
 const NOT_FOUND = require('./utils/errors/NOT_FOUND');
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateRegister } = require('./utils/validators/userValidators');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT } = require('./config');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
+app.use(requestLogger);
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateRegister, createUser);
@@ -24,6 +26,8 @@ app.use('/', routes);
 app.use('*', (req, res, next) => {
   next(new NOT_FOUND('Page not found'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
